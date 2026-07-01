43cb419ff200651cb8f0496fa26a24a8565e2b54
giaphust <test@gmail.com>
2026-07-01 19:19:20 +0700

    update

--- OrderService1.java
+++ OrderService1.java
@@ -2,9 +2,6 @@ import java.util.List;
 
 public class OrderService1 {
 
-    /**
-     * Calculate total order price
-     */
     public double calculateSubtotal(List<Double> prices, double taxRate) {
         double subtotal = 0;
         for (Double price : prices) {
